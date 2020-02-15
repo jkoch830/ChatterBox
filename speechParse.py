18e62bec7889929ef29072bbbd25df23459c9d86
Carolyn Lee <cnl2@andrew.cmu.edu>
2020-02-15 02:07:56 -0500

    Create speechParse.py

---
+++ speechParse.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+import retinasdk
+liteClient = retinasdk.LiteClient("6ea5d540-4fb8-11ea-8f72-af685da1b20e")
+
+
+print (liteClient.getKeywords ("Python is a widely used general-purpose, high-level programming language.Now relax a few minutes (up to 5) until your key is activated or have a look at our Quick Start Guide for the REST API and the client SDKs for Java, Python and Javascript.Your free API key allows y"))
