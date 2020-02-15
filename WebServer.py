810cb7e9a44100ea346f965d8dd861675e01237b
James Koch <jkoch830@gmail.com>
2020-02-14 23:03:30 -0500

    test server

--- WebServer.py
+++ WebServer.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template, Response, jsonify
 
 app = Flask(__name__)
 
+# chatterboxweb.heroku.web.com
+# https://chatterboxweb.herokuapp.com/
 
-@app.route("/")
+
+@app.route("/", methods=['POST'])
 def hello_world():
+    content = request.get_json()
+    print(content)
     return "Hello, World"
+
+
