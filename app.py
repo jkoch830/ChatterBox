31026e2998818da208d59cc3ba21c4abace689c7
Edward <lued2018@gmail.com>
2020-02-15 13:07:20 -0500

    push

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, redirect, url_for, render_template, Response, jsonify
+import pyrebase
+
+config = {
+    "apiKey": "4XqGkMlhGvO3sIr3PFliJbblTdfhM8O0NYAVnItsiJM",
+    "authDomain": "chatterbox-83fc3.firebaseapp.com",
+    "databaseURL": "https://chatterbox-83fc3.firebaseio.com",
+    "storageBucket": "chatterbox-83fc3.appspot.com"
+}
+
+p_firebase = pyrebase.initialize_app(config)
+
+def post_data(user, friend, key_words, emotion, photo):
+    storage = p_firebase.storage()
+    storage.child("friends/example.jpg").put(photo)
+
+    data = {'Key Words': key_words, 'Emotion': emotion}
+    if not SearchDatabase.user_in_database(user):   # user not in database
+        print("BEFORE POST")
+        firebase_database.post(ENTRY_URL + user + '/' + friend + '/', data)
+        print("AFTER POST")
+    elif SearchDatabase.current_friend_of_user(user, friend):
+        current_key_words = SearchDatabase.get_key_words(user, friend)
+        edited = list(set(current_key_words + key_words))  # removes
+        UpdateDatabase.update(user, friend, edited, emotion)
+    else:       # user is in database but friend is not
+        firebase_database.post(ENTRY_URL + user + '/' + friend + '/', data)
+
+
+#post_data("Edward", "Sophia", ['fifth', 'first'], 'awake')
+#SearchDatabase.search_database('Edward')
+#SearchDatabase.user_in_database("Edward")
+
+@app.route("/enter", methods=['POST'])
+def enter_new_conversation():
+    if request.method == 'POST':    # new conversation
+        data = request.form
+        user = data['user']
+        friend = data['friend']
+        conversation = data['conversation']
+        photo = request.files.get("profilePicture")
+        print(photo)
+        b = io.BytesIO(photo.read())
+        key_words = SpeechParse.get_key_words(conversation)
+        emotion = EmotionScanner.get_emotion(conversation)
+        post_data(user, friend, key_words, emotion, b)   # posts data
+    data = {"success": True}
+    return jsonify(data)
+
+
+@app.route("/retrieve", methods=['POST'])
+def retrieve_data():
+    data = {"failed": False}
+    if request.method == 'POST':            # retrieve_data
+        info = request.form
+        user = info['user']
+        friend = info['friend']
+        if SearchDatabase.user_in_database(user):
+            data = SearchDatabase.search_database(user)
+    return jsonify(data)
+
+#res = SearchDatabase.search_database('Edward')
+#print(res)
