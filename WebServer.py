737b292102a4f983f1e84c0003433cb911bb5f56
James Koch <jkoch830@gmail.com>
2020-02-15 02:14:34 -0500

    database

--- WebServer.py
+++ WebServer.py
@@ -1,15 +1,39 @@
 from flask import Flask, request, redirect, url_for, render_template, Response, jsonify
 
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase import firebase
+
+
+# https://chatterboxweb.herokuapp.com/
 app = Flask(__name__)
 
-# chatterboxweb.heroku.web.com
-# https://chatterboxweb.herokuapp.com/
+DATABASE_URL = "https://chatterbox-83fc3.firebaseio.com/"
+firebase_database = firebase.FirebaseApplication(DATABASE_URL, None)
 
 
-@app.route("/", methods=['POST'])
-def hello_world():
-    content = request.get_json()
-    print(content)
+
+
+
+@app.route("/", methods=['POST', 'GET'])
+def enter_new_conversation():
+    if request.method == 'POST':    # new conversation
+        data = request.json
+        user = data['user']
+        friend = data['friend']
+        conversation = data['conversation']
+        key_words =
+        post_data(user, friend, key_words)   # posts data
+        return jsonify({'emotion' : '', 'keyWords' : []})
+    elif request.method == 'GET':   # retrieving data
+        data = request.json
+
+        user = data['user']
+        other = data['other']
+        mp3 = data['mp3']
+        ref = db
     return "Hello, World"
 
 
