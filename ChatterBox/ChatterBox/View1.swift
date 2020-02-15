552ab964441c0902318b3280936ab233e578d236
Sophia Lau <slau2022@gmail.com>
2020-02-14 23:26:32 -0500

    uhhh swipey

---
+++ ChatterBox/ChatterBox/View1.swift
@@ -0,0 +1,30 @@
+//
+//  View1.swift
+//  ChatterBox
+//
+//  Created by Sophia Lau on 2/14/20.
+//  Copyright © 2020 Edward. All rights reserved.
+//
+
+import UIKit
+
+class View1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ChatterBox/ChatterBox/ViewController.swift
+++ ChatterBox/ChatterBox/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        var V1: View1 = View1(nibName: "View1", bundle: nil)
+        
+        var V2: View2 = View2(nibName: "View2", bundle: nil)
+        
+        self.addChild(V1)
+        self.scrollView.addSubview(V1.view)
+        V1.didMove(toParent: self)
+        
+        self.addChild(V2)
+        self.scrollView.addSubview(V2.view)
+        V2.didMove(toParent: self)
+        
+        var V2Frame : CGRect = V2.view.frame
+        V2Frame.origin.x = self.view.frame.width
+        V2.view.frame = V2Frame
+        
+        self.scrollView.contentSize = CGSize(width: self.view.frame.width*2, height: self.view.frame.size.height)
+        
+        
+    
+    
     }
 
 
