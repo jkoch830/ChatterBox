8bd57995aac2a13c2624544447fc29f278904ef4
James Koch <jkoch830@gmail.com>
2020-02-15 16:57:03 -0500

    sort

--- app.py
+++ app.py
@@ -89,6 +89,7 @@ def retrieve_data():
             friends = data.keys()
             print("DATA", data)
             friends = data.keys()
+            sort(friends)
             print("FRIENDS: ", friends)
             for friend in friends:
                 name = user + "_" + friend + ".jpg"
